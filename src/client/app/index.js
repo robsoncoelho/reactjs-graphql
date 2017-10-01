fe9a55b3450b695590a1623308891a37cca98d46
robsoncoelho <robson.amc@gmail.com>
2017-10-01 03:35:43 -0300

    change hashRouter to browserRouter; add http-server

--- src/client/app/index.js
+++ src/client/app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 
 import { CLIENT } from './Components/Common/api.js';
@@ -11,23 +11,19 @@ import SearchBox from './Components/SearchBox/';
 import Products from './Components/Products/';
 import style from './Components/Common/style.scss';
 
-class App extends Component {
-  render () {
-    return (
-    	<ApolloProvider client={CLIENT}>
-	    	<Router>
-		    	<div className={style.main}>
-		    		<Header />
-		    		<section className={style.container}>
-						<Route exact path="/" component={SearchBox}/>
-						<Route path="/products" component={Products}/>
-		    		</section>
-		    		<Footer />
-		    	</div>
-		    </Router>
-		</ApolloProvider>
-    )
-  }
-}
+const App = () => (
+	<ApolloProvider client={CLIENT}>
+    	<Router>
+	    	<div className={style.main}>
+	    		<Header />
+	    		<section className={style.container}>
+					<Route exact path="/" component={SearchBox}/>
+					<Route path="/products" component={Products}/>
+	    		</section>
+	    		<Footer />
+	    	</div>
+	    </Router>
+	</ApolloProvider>
+)
 
 render(<App/>, document.getElementById('app'));
